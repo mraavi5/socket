3de1e99243563b85d96c09cb863568318611bb35
simewu <swuthier@uccs.edu>
2023-06-20 19:11:55 -0600

    Add [200~-std=c++17~

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-g++ server.cpp -I redisclient/src -L/usr/local/lib -o server -lpthread -lboost_system -lhiredis -lredis++ -lcrypto
-g++ client.cpp -I redisclient/src -L/usr/local/lib -o client -lpthread -lboost_system -loqs -lcrypto
-g++ database_filler.cpp -I redisclient/src -L/usr/local/lib -o database_filler -loqs -lhiredis -lredis++ -lcrypto
+g++ server.cpp -std=c++17 -I redisclient/src -L/usr/local/lib -o server -lpthread -lboost_system -lhiredis -lredis++ -lcrypto
+g++ client.cpp -std=c++17 -I redisclient/src -L/usr/local/lib -o client -lpthread -lboost_system -loqs -lcrypto
+g++ database_filler.cpp -std=c++17 -I redisclient/src -L/usr/local/lib -o database_filler -loqs -lhiredis -lredis++ -lcrypto
 
 echo "Done!"
