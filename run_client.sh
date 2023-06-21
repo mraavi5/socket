c9ddb459342841f9eac9d530e19e1388438c2c7b
simewu <swuthier@uccs.edu>
2023-06-20 19:21:58 -0600

    Improved run_client.sh

--- run_client.sh
+++ run_client.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-./client 127.0.0.1 https://www.google.com
+# Check if first argument exists, otherwise default to 127.0.0.1
+if [ -n "$1" ]
+then
+    IP=$1
+else
+    IP="127.0.0.1"
+fi
+
+# Check if second argument exists, otherwise default to https://www.google.com
+if [ -n "$2" ]
+then
+    DOMAIN=$2
+else
+    DOMAIN="https://www.google.com"
+fi
+
+# Print the selected IP and domain
+echo "Selected IP: $IP"
+echo "Selected domain: $DOMAIN"
+
+# Run the client with the selected IP and domain
+./client $IP $DOMAIN
