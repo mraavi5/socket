17fa4c45e52f8a59b59f27a5c6ea924c461be5ec
simewu <swuthier@uccs.edu>
2023-06-20 19:10:06 -0600

    Add -lpthread

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-g++ server.cpp -I redisclient/src -L/usr/local/lib -o server -lboost_system -lhiredis -lredis++ -lcrypto
-g++ client.cpp -I redisclient/src -L/usr/local/lib -o client -lboost_system -loqs -lcrypto
+g++ server.cpp -I redisclient/src -L/usr/local/lib -o server -lpthread -lboost_system -lhiredis -lredis++ -lcrypto
+g++ client.cpp -I redisclient/src -L/usr/local/lib -o client -lpthread -lboost_system -loqs -lcrypto
 g++ database_filler.cpp -I redisclient/src -L/usr/local/lib -o database_filler -loqs -lhiredis -lredis++ -lcrypto
 
 echo "Done!"
